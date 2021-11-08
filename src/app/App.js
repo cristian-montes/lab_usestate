7ff35d6d0f19803adc788d314172f5017fc9c2f1
Cristian Montes <cmontesmujica@gmail.com>
2021-11-08 14:59:30 -0700

    added QuotesContainer

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import QuotesContainer from '../containers/QuotesContainer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <QuotesContainer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/QuotesContainer.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const QuotesContainer = () => {
+    return(
+        <h1>ALL QUOTES WILL GO HERE</h1>
+    )
+}
+
+
+
+
+
+export default QuotesContainer;
